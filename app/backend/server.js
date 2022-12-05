d7f27767669f58a3df43fad8db1bc5ca5928adb3
Dejan <imdejan00@gmail.com>
2022-12-05 03:55:29 +0100

    Updated dependencies aswell as server code

--- app/backend/server.js
+++ app/backend/server.js
@@ -9,8 +9,11 @@ const connString = process.env.MONGO_STRING
 const PORT = process.env.PORT
 
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use('/users', require('./routers/users_router'))
 
 app.listen(PORT, () => {
     console.log(`This does indeed work! Running on port ${PORT}`)
